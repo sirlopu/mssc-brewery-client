4f18da3869cf36eec7fcb8ec3eeba7dd2cab58c5
Gabriel S <sirlopug@gmail.com>
2020-11-15 15:40:44 -0800

    Implemented Rest template and initial test

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # MSSC Brewery Client
+
+API Doc: https://sfg-beer-works.github.io/brewery-api/
---
+++ src/test/java/com/sirlopu/msscbreweryclient/web/client/BreweryClientTest.java
@@ -0,0 +1,24 @@
+package com.sirlopu.msscbreweryclient.web.client;
+
+import com.sirlopu.msscbreweryclient.web.model.BeerDto;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class BreweryClientTest {
+
+    @Autowired
+    BreweryClient client;
+
+    @Test
+    void getBeerById() {
+        BeerDto dto = client.getBeerById(UUID.randomUUID());
+
+        assertNotNull(dto);
+    }
+}
